68806af1d248a9c7f990f5cbd3af48fb5fc02b78
Arjun Varma <arj25835@gmail.com>
2026-05-12 16:24:09 +0100

    Fixed clangd intellisense

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,30 @@
 return {
-    { "neovim/nvim-lspconfig" },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                clangd = {
+                    root_dir = function(fname)
+                        local util = require("lspconfig.util")
+                        return util.root_pattern(
+                            ".clangd",
+                            ".clang-tidy",
+                            "compile_commands.json",
+                            "compile_flags.txt",
+                            "CMakeLists.txt",
+                            ".git"
+                        )(fname)
+                    end,
+                    cmd = {
+                        "clangd",
+                        "--background-index",
+                        "--clang-tidy",
+                        "--completion-style=detailed",
+                        "--header-insertion=iwyu",
+                        "--query-driver=/opt/homebrew/bin/clang++,/opt/homebrew/bin/clang,/usr/bin/clang,/usr/bin/clang++",
+                    },
+                },
+            },
+        },
+    },
 }
