0e5c4b86527a1d73076cca08d67ee0bbadd58f5b
Arjun Varma <arj25835@gmail.com>
2026-06-13 11:51:22 +0100

    Added now lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,17 +4,6 @@ return {
         opts = {
             servers = {
                 clangd = {
-                    root_dir = function(fname)
-                        local util = require("lspconfig.util")
-                        return util.root_pattern(
-                            ".clangd",
-                            ".clang-tidy",
-                            "compile_commands.json",
-                            "compile_flags.txt",
-                            "CMakeLists.txt",
-                            ".git"
-                        )(fname)
-                    end,
                     cmd = {
                         "clangd",
                         "--background-index",
